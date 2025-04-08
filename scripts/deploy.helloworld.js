9af852ab63fc428d03913268da8663a2c8d654df
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-04-08 19:36:27 +0530

    update

---
+++ scripts/deploy.helloworld.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
+  const helloWorld = await HelloWorld.deploy("Hello, World!");
+
+  await helloWorld.deployed(); 
+
+  console.log("Deployed HelloWorld contract at:", helloWorld.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
